ff7da209afeb08341f734b0669e09d6053bc6418
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-12-17 18:58:49 +0400

    ADD calcTable in data.alpine.js

--- src/js/data.alpine.js
+++ src/js/data.alpine.js
@@ -7,6 +7,7 @@ import { footer } from './alpine/footer.js';
 import { sorting } from './alpine/sorting.js';
 import { complectation } from './alpine/complectation.js';
 import { store } from './alpine/store.js';
+import { calcTable } from './alpine/calcTable.js';
 
 document.addEventListener('alpine:init', () => {
 	usedPreviewGallery();
@@ -16,6 +17,7 @@ document.addEventListener('alpine:init', () => {
 	sorting();
 	complectation();
 	store();
+	calcTable();
 });
 
 // window.Alpine = Alpine;
